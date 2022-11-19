dc3ea2e7465b4bf8bdbdfdae8ebeaf2a24df3a4b
вадим конько <frostvim@mail.ru>
2022-11-19 22:06:20 +0300

    Добавьте файлы проекта.

---
+++ Kpyap 11/Catalog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kpyap_11
+{
+    
+    class Catalog
+    {
+        
+        private string name;
+        private DateTime timeOfStart;
+        private int countOfFiles;
+
+        public string Name
+        {
+            get { return name; }
+            set {  name = value; }
+        }
+        public DateTime TimeOfStart
+        {
+            get { return timeOfStart; }
+            set 
+            {
+                DateTime maxDate = DateTime.Now;
+                if (value > maxDate)
+                {
+                    timeOfStart = maxDate;
+                }
+                else
+                {
+                    timeOfStart = value;
+                }
+                
+            }
+        }
+        public int CountOfFiles
+        {
+            get { return countOfFiles; }
+            set 
+            {
+                if (value < 0)
+                {
+                    countOfFiles = 1;
+                }
+                else
+                {
+                    countOfFiles = value;
+                }
+                
+            }
+        }
+    }
+}
---
+++ Kpyap 11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Kpyap_11
+{
+    class Program
+    {
+        
+        public static int Menu(string text)
+        {
+            Console.WriteLine(text);
+            int coise = Convert.ToInt32(Console.ReadLine());
+            return coise;
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                S
+            }
+            
+        }
+    }
+}
